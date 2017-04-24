cea20c8e40347f35430b4848520e8293c503f956
AnnaAmalfi94 <annamalfi@gmail.com>
2017-04-24 16:59:00 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'newsweb',
+		wp_ws_url : 'http://newsweb.annamalfi.it/wp-appkit-api/newsweb',
+		wp_url : 'http://newsweb.annamalfi.it',
+		theme : 'newsweb',
+		version : '0.1',
+		app_title : 'Newsweb',
+		app_platform : 'android',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : 'w&041@PA}IuZIBHml>zRlDb-Bg~r9}16p- MSefGZX*<+W!?R]UzUNVfw7r9}F-X',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
